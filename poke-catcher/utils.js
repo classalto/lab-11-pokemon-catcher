365535b0fa6216d86ed995010f4237aa6e88b9a6
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-01-26 19:43:48 -0800

    fix paths

--- poke-catcher/utils.js
+++ poke-catcher/utils.js
@@ -55,7 +55,7 @@ export function renderPokemonImg(pokemonInstance) {
         if (gamesPlayed <= 10) {
             setRandomPokemon();
         } else {
-            window.location.href = '../results/results.html';
+            window.location.href = '../results/result-page.html';
         }
     });
     return image;
